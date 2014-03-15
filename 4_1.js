c4973e0ae7e1b3a1d56f3227a312918baafd26e6
John Kasiewicz <crestcode@gmail.com>
2014-03-14 23:54:54 -0400

    chapter 4

--- 4_1.js
+++ 4_1.js
@@ -79,3 +79,42 @@ function missingParen(expression) {
 
 missingParen("2*(3+4");
 missingParen("2*(2+4)");
+
+function convertInfixToPostfix(infix) {
+    var s = new Stack();
+    var postfix = "";
+    if (missingParen(infix)) {
+        return "Invalid expression: Check parens"
+    } else {
+        for (var i = 0; i < infix.length; ++i) {
+            if (isNaN(infix[i])) {
+                if (infix[i] == ")")
+                    postfix += s.pop();
+                else if (infix[i] == "+" || infix[i] == "-") {
+                    if (s.length() != 0 && s.peek() == "(") {
+                        s.pop();
+                        s.push(infix[i]);
+                    } else if (s.length() != 0 && (s.peek() == "*" || s.peek() == "/")) {
+                        while (s.length() > 0)
+                            postfix += s.pop();
+                        s.push(infix[i]);
+                    } else {
+                        s.push(infix[i]);
+                    }
+                } else if (infix[i] == "(" || infix[i] == "*" || infix[i] == "/")
+                    s.push(infix[i]);
+            } else {
+                postfix += infix[i];
+            }
+        }
+        while (s.length() > 0) {
+            if (s.peek() == "(")
+                s.pop();
+            else
+                postfix += s.pop();
+        }
+        return postfix;
+    }
+}
+
+convertInfixToPostfix("((4-1)/(2+12))+1*(4/3)");
