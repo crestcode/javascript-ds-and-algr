0fdfe534e340b8cc4f785ffbfb61dd0d8b7e4d9c
John Kasiewicz <crestcode@gmail.com>
2014-04-04 15:22:00 -0400

    chapter 7

---
+++ 7.js
@@ -0,0 +1,26 @@
+function Dictionary() {
+    this.add = add;
+    this.datastore = new Array();
+    this.find = find;
+    this.remove = remove;
+    this.showAll = showAll;
+}
+function add(key, value) {
+    this.datastore[key] = value;
+}
+function find(key) {
+    return this.datastore[key];
+}
+function remove(key) {
+    delete this.datastore[key];
+}
+function showAll() {
+    for each (var key in Object.keys(this.datastore)) {
+        print(key + " -> " + this.datastore[key]); }
+}
+
+var pbook = new Dictionary(); pbook.add("Mike","123");
+pbook.add("David", "345");
+pbook.add("Cynthia", "456");
+print("David's extension: " + pbook.find("David")); pbook.remove("David");
+pbook.showAll();
