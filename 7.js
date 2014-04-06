65bea1d188a37367fc33874b5e8c7751fcd375c7
John Kasiewicz <crestcode@gmail.com>
2014-04-06 15:22:00 -0400

    enhance dictionary

--- 7.js
+++ 7.js
@@ -4,23 +4,45 @@ function Dictionary() {
     this.find = find;
     this.remove = remove;
     this.showAll = showAll;
+    this.count = count;
+    this.clear = clear;
 }
+
 function add(key, value) {
     this.datastore[key] = value;
 }
+
 function find(key) {
     return this.datastore[key];
 }
+
 function remove(key) {
     delete this.datastore[key];
 }
+
 function showAll() {
     for each (var key in Object.keys(this.datastore)) {
         print(key + " -> " + this.datastore[key]); }
 }
 
-var pbook = new Dictionary(); pbook.add("Mike","123");
+function count() {
+    var n = 0;
+    for each (var key in Object.keys(this.datastore)) {
+        ++n;
+    }
+    return n;
+}
+function clear() {
+    for each (var key in Object.keys(this.datastore)) {
+        delete this.datastore[key];
+    }
+}
+
+var pbook = new Dictionary(); pbook.add("Raymond","123");
 pbook.add("David", "345");
 pbook.add("Cynthia", "456");
-print("David's extension: " + pbook.find("David")); pbook.remove("David");
+print("Number of entries: " + pbook.count());
+print("David's extension: " + pbook.find("David"));
 pbook.showAll();
+pbook.clear();
+print("Number of entries: " + pbook.count());
